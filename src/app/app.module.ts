686a28f3acbf457bf8f243a8c1b1e8b4635e1b16
ymougenel <yann.mougenel@gmail.com>
2020-03-29 21:53:41 +0200

    feat(UI): filter displayed countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     ChartsModule,
     HttpClientModule,
     AppRoutingModule,
