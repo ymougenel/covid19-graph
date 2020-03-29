246b3c5bfe57bd3540f4758f0feed94b7c7587bb
ymougenel <yann.mougenel@gmail.com>
2020-03-29 20:10:57 +0200

    refactor: rework import & structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { ContaminatedGraphComponent } from './contaminated-graph/contaminated-graph.component';
-import { MessageService } from './core/services/message.service';
 import { AppRoutingModule } from './app-routing.module';
-import {FormsModule} from "@angular/forms";
-import {DataService} from "./core/services/data.service";
-import {MessagesComponent} from "./messages/messages.component";
+import { AppComponent } from './app.component';
+import { GraphComponent } from './graph/graph.component';
+import {HttpClientModule} from "@angular/common/http";
 import {ChartsModule} from "ng2-charts";
-import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
-import {FormatterService} from "./core/services/formatter.service";
-import {MapperService} from "./core/services/mapper.service";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContaminatedGraphComponent,
-    MessagesComponent,
-
+    GraphComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ChartsModule,
-    CoreModule,
     HttpClientModule,
     AppRoutingModule,
+    FontAwesomeModule
   ],
-  providers: [MessageService, DataService, FormatterService, MapperService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/graph/graph.component.spec.ts
+++ src/app/graph/graph.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MessagesComponent } from './messages.component';
+import { GraphComponent } from './graph.component';
 
-describe('MessagesComponent', () => {
-  let component: MessagesComponent;
-  let fixture: ComponentFixture<MessagesComponent>;
+describe('GraphComponent', () => {
+  let component: GraphComponent;
+  let fixture: ComponentFixture<GraphComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessagesComponent ]
+      declarations: [ GraphComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MessagesComponent);
+    fixture = TestBed.createComponent(GraphComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
